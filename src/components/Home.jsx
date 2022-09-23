37e9e4be2e9c6af194410c6bf6cf1fbc7170ba25
Daniel <daniel_ye.nz@hotmail.com>
2022-09-23 03:55:13 -0700

    buttons for easier access to pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 const Home = () => {
+
   return (
-    <div>Home</div>
+    <div>Home
+    <Link to="./Artists/"><button >Artists</button></Link>
+    <Link to="./Albums/"><button >Albums</button></Link>
+
+    </div>
+
   )
 }
 
