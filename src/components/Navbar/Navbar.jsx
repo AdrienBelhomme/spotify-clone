b055966678a21bc5afcf9f4252a114bf0bea4430
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-09-24 12:55:02 -0400

    Trail

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>NavBar Finished</div>
+  );
+};
 
-export default Navbar
+export default Navbar;
