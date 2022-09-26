3e2b51c64a2d8b840e20a5f30543c6e86016f708
Adrien <belhommeadrien@gmail.comn>
2022-09-26 14:25:59 +0200

    fix minor eslint errors

--- src/assets/genres/index.js
+++ src/assets/genres/index.js
@@ -1,11 +1,11 @@
-import home from '../genres/music-library-removebg-preview.png';
-import artist from '../genres/james-brown-removebg-preview.png';
-import album from '../genres/music-record-removebg-preview.png';
-import topchart from '../genres/radio-removebg-preview.png';
+import home from './music-library-removebg-preview.png';
+import artist from './james-brown-removebg-preview.png';
+import album from './music-record-removebg-preview.png';
+import topchart from './radio-removebg-preview.png';
 
 export default {
-    home,
-    artist,
-    album,
-    topchart
+  home,
+  artist,
+  album,
+  topchart,
 };
