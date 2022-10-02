cb8fbbf66169adcb4ce19964c615a1ea1219e262
C1okesta <73605636+C1okesta@users.noreply.github.com>
2022-10-02 11:08:02 -0700

    artists dettts

--- src/components/Artists.jsx
+++ src/components/Artists.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 
 const Artists = () => {
-  const { id } = useParams();
-
   return (
     <div>Artists</div>
   )
 }
 
 export default Artists
-
-export default Artists;
