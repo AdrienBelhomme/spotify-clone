5ac247b5f0365bebfef25131a305743c90c0fa49
C1okesta <73605636+C1okesta@users.noreply.github.com>
2022-10-02 14:35:46 -0700

    artists work

--- src/components/Artists.jsx
+++ src/components/Artists.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box, Button, CircularProgress, Grid,Typography } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import { ArrowBack } from '@mui/icons-material';
+
+import { useGetArtistDetailsQuery } from '../services/shazam.js';
 
 const Artists = () => {
+ const { id } = useParams();
+
+ const { data, isFetching, error } = useGetArtistDetailsQuery(id);
+
+ if (isFetching) {
   return (
-    <div>Artists</div>
+    <Box display="flex" justifyContent="center">
+      <CircularProgress size="8rem" />
+    </Box>
+  );
+}
+
+if (error) {
+  console.log(error);
+  return (
+    <Box display="flex" justifyContent="center" alignItems="center">
+    <Button startIcon={<ArrowBack />} onClick={() => goBack()} color="primary">
+    Go Back
+      </Button>
+      </Box>
+  );
+}
+
+  return (
+    <div>Artists {id}
+    </div>
   )
 }
 
