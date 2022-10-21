195e9c853240f91bc529ea959429b3c9bf80f9a7
Daniel <daniel_ye.nz@hotmail.com>
2022-10-20 22:16:03 -0700

    add some css to albums page

--- src/components/CardAlbum.jsx
+++ src/components/CardAlbum.jsx
@@ -5,7 +5,10 @@ import { useNavigate, Link } from 'react-router-dom';
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
-  padding: '20px 20px',
+  padding: '10px 30px',
+  width: '300px',
+  height: '330px',
+  margin: '10px',
   textAlign: 'center',
   background: 'transparent',
   color: theme.palette.text.secondary,
@@ -33,7 +36,7 @@ const CardAlbum = ({ song }) => {
             height="180px"
             style={{ borderRadius: '15px' }}
           />
-          <h3 style={{ textAlign: 'left', color: '#2E3271', fontWeight: '600', fontSize: '16px', margin: '12px 0',
+          <h3 style={{ textAlign: 'center', color: '#2E3271', fontWeight: '600', fontSize: '16px', margin: '12px 0',
           }}
           >
             <Link to={`../SongDetails/${song?.key}`}>
